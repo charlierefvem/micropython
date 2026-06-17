64f963b9d12d7476659a49988fe3a621d255c25e
Charlie Refvem <crefvem@calpoly.edu>
2026-06-17 15:33:50 -0700

    Workflow further modified to allow the micropython optimization level to be set during freezing by setting the repository variable MICROPY_FROZEN_OPT_LEVEL to 0, 1, 2, or 3

--- ports/stm32/boards/NUCLEO_L476RG/manifest.py
+++ ports/stm32/boards/NUCLEO_L476RG/manifest.py
@@ -1,2 +1,9 @@
+import os
+
 include("$(PORT_DIR)/boards/manifest.py")
-freeze("$(BOARD_DIR)/modules")
+
+_frozen_opt_level = int(os.environ.get("MICROPY_FROZEN_OPT_LEVEL", "0"))
+if _frozen_opt_level < 0 or _frozen_opt_level > 3:
+    raise ValueError("MICROPY_FROZEN_OPT_LEVEL must be 0, 1, 2, or 3")
+
+freeze("$(BOARD_DIR)/modules", opt=_frozen_opt_level)
